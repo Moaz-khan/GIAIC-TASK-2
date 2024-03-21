648cac69324894f893ad83ff4e7315e2557d28a5
Moaz-khan <kmoaz614@gmail.com>
2024-03-21 12:21:43 -0700

    GIAIC task 2

---
+++ task2.js
@@ -0,0 +1,4 @@
+//task 2
+// Person Message:
+var personname = "eric";
+console.log("Hello ".concat(personname, " would you like learn some python today"));
---
+++ task2.ts
@@ -0,0 +1,6 @@
+//task 2
+// Person Message:
+let personname : string = "eric";
+
+console.log(`Hello ${personname} would you like learn some python today`);
+
